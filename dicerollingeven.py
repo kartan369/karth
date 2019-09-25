0c253056ae75296bb3bc7035e8ab48e52a91aacf
kartan369 <karthik9reddy9@gmail.com>
2019-09-24 20:14:17 -0400

    dice rolling simulator only even

---
+++ dicerollingeven.py
@@ -0,0 +1,18 @@
+import random
+min = 1
+max = 6
+a=b=c=0
+roll_again = "yes"
+while roll_again == "yes" or roll_again == "y":
+    print("Rolling the dices...")
+    print("The values are....")
+    a = random.randint(min, max)
+    b = random.randint(min, max)
+    print(b)
+    print(a)
+    c = a+b
+    if c%2==0:
+        print("c is an even number ",(c))
+        exit()
+    elif c%2==1:
+        print("c is an odd number",(c))
